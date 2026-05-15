caab599d79f8f615b3b9615977f92fae2e514767
Anton <a.ambaroff@gmail.com>
2026-05-15 09:12:13 +0500

    create migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260515_041146 from './20260515_041146';
+
+export const migrations = [
+  {
+    up: migration_20260515_041146.up,
+    down: migration_20260515_041146.down,
+    name: '20260515_041146'
+  },
+];
