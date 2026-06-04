9fa5824a8f6556e35b423f48a9a4a8552850a0cd
Anton <a.ambaroff@gmail.com>
2026-06-04 16:39:30 +0500

    fix vercel blob

--- src/payload/collections/Media.ts
+++ src/payload/collections/Media.ts
@@ -18,14 +18,6 @@ export const Media: CollectionConfig = {
 		disableLocalStorage: true,
 		adminThumbnail: 'thumbnail',
 		focalPoint: true,
-		imageSizes: [
-			{ name: 'thumbnail', width: 300 },
-			{ name: 'small', width: 600 },
-			{ name: 'medium', width: 900 },
-			{ name: 'large', width: 1400 },
-			{ name: 'xlarge', width: 1920 },
-			{ name: 'og', width: 1200, height: 630, crop: 'center' },
-		],
 	},
 	fields: [
 		{
